ad8e93ebdc28e0de57172e388873f4fc23897dd9
Tuomas Kulve <tuomas@kulve.fi>
2012-01-11 17:15:47 +0200

    Added Snowcap Control Board v1.

---
+++ boards/SNOWCAP_CONTROL_BOARD_V1/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/boards/SNOWCAP_CONTROL_BOARD_V1/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/boards/SNOWCAP_CONTROL_BOARD_V1
